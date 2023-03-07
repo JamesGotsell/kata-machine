b752cfe555656d7ce99fb4417b65f97ee5c70237
James Gotsell <j.e.gotsell@gmail.com>
2023-03-07 12:18:50 +0000

    implemented bubble sort

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,16 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    // sqrt of n - breaks.length 
     const jmpAmount = Math.floor(Math.sqrt(breaks.length));
 
     let i = jmpAmount;
-
-    for (; i < breaks.length; i += jmpAmount) {
+//  find where it breaks 
+    for(; i < breaks.length; i += jmpAmount) {
         if(breaks[i]) {
             break;
         }
     }
+// jump back sqrt of n 
     i -= jmpAmount;
 
+// linerally walk sqrt of n until we have a break - index which it breaks 
     for(let j = 0; j < jmpAmount && i < breaks.length; ++j, ++i) {
         if(breaks[i]) {
             return i;
